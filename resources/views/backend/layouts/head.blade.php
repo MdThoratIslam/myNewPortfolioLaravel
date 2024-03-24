0cc1b746a16c0ecd30ba8b7a222371a130e1e658
Md Thorat Islam <mdthoratislam1995.oni@gmail.com>
2024-03-24 14:10:11 +0600

    Dashboard Path Edit

--- resources/views/backend/layouts/head.blade.php
+++ resources/views/backend/layouts/head.blade.php
@@ -13,19 +13,19 @@
     <meta content="" name="Md Thorat Islam" />
     <!-- GOOGLE FONTS -->
     <link href="https://fonts.googleapis.com/css?family=Karla:400,700|Roboto" rel="stylesheet">
-    <link href="{{asset('public/backend/')}}/plugins/material/css/materialdesignicons.min.css" rel="stylesheet" />
-    <link href="{{asset('public/backend/')}}/plugins/simplebar/simplebar.css" rel="stylesheet" />
+    <link href="{{asset('public/backend/')}}plugins/material/css/materialdesignicons.min.css" rel="stylesheet" />
+    <link href="{{asset('public/backend/')}}plugins/simplebar/simplebar.css" rel="stylesheet" />
     <!-- PLUGINS CSS STYLE -->
-    <link href="{{asset('public/backend/')}}/plugins/nprogress/nprogress.css" rel="stylesheet" />
-    <link href="{{asset('public/backend/')}}/plugins/DataTables/DataTables-1.10.18/css/jquery.dataTables.min.css" rel="stylesheet" />
-    <link href="{{asset('public/backend/')}}/plugins/jvectormap/jquery-jvectormap-2.0.3.css" rel="stylesheet" />
-    <link href="{{asset('public/backend/')}}/plugins/daterangepicker/daterangepicker.css" rel="stylesheet" />
+    <link href="{{asset('public/backend/')}}plugins/nprogress/nprogress.css" rel="stylesheet" />
+    <link href="{{asset('public/backend/')}}plugins/DataTables/DataTables-1.10.18/css/jquery.dataTables.min.css" rel="stylesheet" />
+    <link href="{{asset('public/backend/')}}plugins/jvectormap/jquery-jvectormap-2.0.3.css" rel="stylesheet" />
+    <link href="{{asset('public/backend/')}}plugins/daterangepicker/daterangepicker.css" rel="stylesheet" />
     <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
-    <link href="{{asset('public/backend/')}}/plugins/toaster/toastr.min.css" rel="stylesheet" />
+    <link href="{{asset('public/backend/')}}plugins/toaster/toastr.min.css" rel="stylesheet" />
     <!-- MONO CSS -->
     <link id="main-css-href" rel="stylesheet" href="{{asset('public/backend/')}}/css/style.css" />
-    <link href="{{asset('public/backend/')}}/images/paper-plane.png" rel="shortcut icon" />
-    <script src="{{asset('public/backend/')}}/plugins/nprogress/nprogress.js"></script>
+    <link href="{{asset('public/backend/')}}images/paper-plane.png" rel="shortcut icon" />
+    <script src="{{asset('public/backend/')}}plugins/nprogress/nprogress.js"></script>
 </head>
 <body class="navbar-fixed sidebar-fixed" id="body">
 <script>
