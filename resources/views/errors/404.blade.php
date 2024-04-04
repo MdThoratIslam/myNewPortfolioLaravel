54035dd1dafc198378f800f2e49ce9cae6a57b80
MdThoratIslam <thorat.pwad03@gmail.com>
2024-04-04 14:34:50 +0600

    404 update

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -42,6 +42,7 @@
         #error {
             color: #fff;
             text-shadow: 0 0 80px #ffffff,0 0 30px #008000,0 0 6px #0000ff;
+            font-size: 18px;
         }
         #error span {
             animation: upper 6s linear infinite;
@@ -71,6 +72,63 @@
             19%,22.99%,32%,36.999%,45%,45.999%,50%,50.99%,59%,60.999%,69%,70.999%,86%,95.999% {
                 opacity:0.4;
                 text-shadow: none;
+            }
+        }
+        // this is for full responsive for all devices max and min use
+        @media (max-width: 768px) {
+            .page-404 {
+                margin-top: 90px;
+                padding: 40px;
+                font-size: 50px;
+                font-family: 'Monoton', cursive;
+                text-align: center;
+                text-transform: uppercase;
+                text-shadow: 0 0 80px red,0 0 30px FireBrick,0 0 6px DarkRed;
+                color: red;
+            }
+            .page-404 p{
+                margin:5px;
+                font-size: 2.98rem; !important;
+            }
+            #error:hover {
+                text-shadow: 0 0 200px #ffffff,0 0 80px #008000,0 0 6px #0000ff;
+            }
+            #code:hover {
+                text-shadow: 0 0 100px red,0 0 40px FireBrick,0 0 8px DarkRed;
+            }
+            #error {
+                color: #fff;
+                text-shadow: 0 0 80px #ffffff,0 0 30px #008000,0 0 6px #0000ff;
+            }
+            #error span {
+                animation: upper 6s linear infinite;
+            }
+            #code span:nth-of-type(2) {
+                animation: lower 9s linear infinite;
+            }
+            #code span:nth-of-type(1) {
+                text-shadow: none;
+                opacity:.4;
+            }
+            @keyframes upper {
+                0%,19.999%,22%,62.999%,64%, 64.999%,70%,100% {
+                    opacity:.99;
+                    text-shadow: 0 0 80px #ffffff,0 0 30px #008000,0 0 6px #0000ff;
+                }
+                20%,21.999%,63%,63.999%,65%,69.999% {
+                    opacity:0.4;
+                    text-shadow: none;
+                }
+            }
+            @keyframes lower {
+                0%,12%,18.999%,23%,31.999%,37%,44.999%,46%,49.999%,51%,58.999%,61%,68.999%,71%,85.999%,96%,100% {
+                    opacity:0.99;
+                    text-shadow: 0 0 80px red,0 0 30px FireBrick,0 0 6px DarkRed;
+                }
+                19%,22.99%,32%,36.999%,45%,45.999%,50%,50.99%,59%,60.999%,69%,70.999%,86%,95.999% {
+                    opacity:0.4;
+                    text-shadow: none;
+                }
             }
         }
 
